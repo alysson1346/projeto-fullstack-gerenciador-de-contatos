3f6c88a85519be2ec1e80ff4b351af80714adbac
unknown <alyssoncolombo@outlook.com>
2022-09-24 01:33:44 -0300

    Config prod

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -11,11 +11,19 @@ export const AppDataSource =
       })
     : new DataSource({
         type: "postgres",
-        host: "localhost",
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PWD,
-        database: process.env.POSTGRES_DB,
-        entities: ["src/entities/**/*.ts"],
-        migrations: ["src/migrations/**/*.ts"],
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: false,
+        logging: true,
+        entities:
+          process.env.NODE_ENV === "production"
+            ? ["dist/entities/*.js"]
+            : ["src/entities/*.ts"],
+        migrations:
+          process.env.NODE_ENV === "production"
+            ? ["dist/migrations/*.js"]
+            : ["src/migrations/*.ts"],
       });
