09de6c7a24e497b15e27b022c44e35bed672bdd6
unknown <alyssoncolombo@outlook.com>
2022-09-26 12:36:30 -0300

    Config prod

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,3 +1,25 @@
+// import { DataSource } from "typeorm";
+// require("dotenv").config();
+
+// export const AppDataSource =
+//   process.env.NODE_ENV === "test"
+//     ? new DataSource({
+//         type: "sqlite",
+//         database: ":memory:",
+//         entities: ["src/entities/**/*.ts"],
+//         synchronize: true,
+//       })
+//     : new DataSource({
+//         type: "postgres",
+//         host: "localhost",
+//         port: 5432,
+//         username: process.env.POSTGRES_USER,
+//         password: process.env.POSTGRES_PWD,
+//         database: process.env.POSTGRES_DB,
+//         entities: ["src/entities/**/*.ts"],
+//         migrations: ["src/migrations/**/*.ts"],
+//       });
+
 import { DataSource } from "typeorm";
 require("dotenv").config();
 
